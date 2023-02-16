f19743f390d07a670bcc52d298a4fb3515d60d29
DRBANDEIRA <91505202+DimmyBandeira@users.noreply.github.com>
2023-02-16 12:31:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+let videoWidth, videoHeight;
+
+// se streaming de vídeo da câmera.
+let streaming = false;
+
+let video = document.getElementById('video');
+let canvasOutput = document.getElementById('canvasOutput');
+let canvasOutputCtx = canvasOutput.getContext('2d');
+let stream = null;
+
+function startCamera() {
+  if (streaming) return;
+  navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(function(s) {
+    stream = s;
+    video.srcObject = s;
+    video.play();
+  })
+    .catch(function(err) {
+    console.log("An error occured! " + err);
+  });
+
+  video.addEventListener("canplay", function(ev){
+    if (!streaming) {
+      videoWidth = video.videoWidth;
+      videoHeight = video.videoHeight;
+      video.setAttribute("width", videoWidth);
+      video.setAttribute("height", videoHeight);
+      canvasOutput.width = videoWidth;
+      canvasOutput.height = videoHeight;
+      streaming = true;
+    }
+    startVideoProcessing();
+  }, false);
+}
+
+let faceClassifier = null;
+
+let src = null;
+let dstC1 = null;
+let dstC3 = null;
+let dstC4 = null;
+
+let canvasInput = null;
+let canvasInputCtx = null;
+
+let canvasBuffer = null;
+let canvasBufferCtx = null;
+
+function startVideoProcessing() {
+  if (!streaming) { console.warn("Inicie sua webcam"); return; }
+  canvasInput = document.createElement('canvas');
+  canvasInput.width = videoWidth;
+  canvasInput.height = videoHeight;
+  canvasInputCtx = canvasInput.getContext('2d');
+  
+  canvasBuffer = document.createElement('canvas');
+  canvasBuffer.width = videoWidth;
+  canvasBuffer.height = videoHeight;
+  canvasBufferCtx = canvasBuffer.getContext('2d');
+  
+  srcMat = new cv.Mat(videoHeight, videoWidth, cv.CV_8UC4);
+  grayMat = new cv.Mat(videoHeight, videoWidth, cv.CV_8UC1);
+  
+  faceClassifier = new cv.CascadeClassifier();
+  faceClassifier.load('haarcascade_frontalface_default.xml');
+    
+  requestAnimationFrame(processVideo);
+}
+
+function processVideo() {
+  canvasInputCtx.drawImage(video, 0, 0, videoWidth, videoHeight);
+  let imageData = canvasInputCtx.getImageData(0, 0, videoWidth, videoHeight);
+  srcMat.data.set(imageData.data);
+  cv.cvtColor(srcMat, grayMat, cv.COLOR_RGBA2GRAY);
+  let faces = [];
+  let eyes = [];
+  let size;
+  let faceVect = new cv.RectVector();
+  let faceMat = new cv.Mat();
+  cv.pyrDown(grayMat, faceMat);
+  cv.pyrDown(faceMat, faceMat);
+  size = faceMat.size();
+  faceClassifier.detectMultiScale(faceMat, faceVect);
+  for (let i = 0; i < faceVect.size(); i++) {
+    let face = faceVect.get(i);
+    faces.push(new cv.Rect(face.x, face.y, face.width, face.height));
+  }
+  faceMat.delete();
+  faceVect.delete();
+  canvasOutputCtx.drawImage(canvasInput, 0, 0, videoWidth, videoHeight);
+  drawResults(canvasOutputCtx, faces, 'red', size);
+  drawResults(canvasOutputCtx, eyes, 'yellow', size);
+  requestAnimationFrame(processVideo);
+}
+
+function drawResults(ctx, results, color, size) {
+  for (let i = 0; i < results.length; ++i) {
+    let rect = results[i];
+    let xRatio = videoWidth/size.width;
+    let yRatio = videoHeight/size.height;
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = color;
+    ctx.strokeRect(rect.x*xRatio, rect.y*yRatio, rect.width*xRatio, rect.height*yRatio);
+  }
+}
+
+function opencvIsReady() {
+  console.log('OpenCV.js is ready');
+  startCamera();
+}
